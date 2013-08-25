13ac9f1bf1df736f8c90c5743b34c6101ce30f06
Dan Newcome <dan@dnuke.com>
2013-08-24 18:21:11 -0700

    cleanup - removing test main file

--- tests/__main__.py
+++
@@ -1,8 +0,0 @@
-import Mime_test 
-import Template_test 
-import unittest
-
-# todo: main() only will run one set of tests for some reason
-# unittest.main(Mime_test)
-unittest.main(Template_test)
-
